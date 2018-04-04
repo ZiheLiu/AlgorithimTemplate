8b953292e410da65b551fdd24291beb5c855ffae
ZiheLiu <ziheliu1024@gmail.com>
2018-04-04 15:35:40 +0800

    add round 959/div2/A

---
+++ practice/codeforces/959-div2-A.cpp
@@ -0,0 +1,18 @@
+//
+// Created by liuzihe on 2018/4/4.
+// Link: http://codeforces.com/contest/959/problem/A.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >>n;
+    if (n & 1) {
+        cout << "Ehab";
+    } else {
+        cout << "Mahmoud";
+    }
+}
